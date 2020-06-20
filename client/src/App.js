685e1755949c97e8b4ef0373add0aea783721550
khaalidm <khaalidm.m@gmail.com>
2020-06-20 20:16:34 +0200

    got final client working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Button from "react-bootstrap/Button"
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Dropdown from "react-bootstrap/Dropdown";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
+import Spinner from "react-bootstrap/Spinner"
 import ChuckGif from "../src/assets/chuck.jpg";
 import Image from "react-bootstrap/Image";
 import { useQuery } from "@apollo/react-hooks";
@@ -30,45 +32,39 @@ const GET_RANDOM_JOKE = gql`
 `;
 
 let check = true;
-let data2;
+let showSpinner = false;
 
 function GetRandomJoke(category) {
-	const { status, data, error, refetch, isFetching } = useQuery(GET_RANDOM_JOKE, {
+	const { loading, error, data, refetch} = useQuery(GET_RANDOM_JOKE, {
 		variables: { category: category.name },
 	});
-	// console.log("~~~~~~~~~~~~~~~~~~~~"+ JSON.stringify(category));
-	if (error) return <h1>Something went wrong!</h1>;
-	// if (loading) return <h1>Loading...</h1>;	
+	if (error) return {};
+	if (loading) return <h1>Loading...</h1>;	
 
 	while (check) {
-		console.log("~~~~~~~im here~~~~~~~~~");
-		// data1 = data;
-		refetch();
 		check =false;
+		showSpinner = true;
+		refetch();
+		
 	}
-
 	return { randomJoke: data };
 }
 
 function App() {
 	const { loading, error, data } = useQuery(GET_CATEGORIES);
-	let [category, setCategory] = useState(false);
+	const [category, setCategory] = useState(false);
 	let { randomJoke } = GetRandomJoke(category);
 	if (error) return <h1>Something went wrong!</h1>;
 	if (loading) return <h1>Loading...</h1>;
 
-	
-	// function resetCategory()
-	// {
-	// 	if(randomJoke?.randomJoke && randomJoke?.randomJoke.category) {
-	// 		console.log({name: randomJoke.randomJoke.category[0]});
-	// 	setCategory({name: randomJoke?.randomjoke?.category[0],  __typename: "Category"});
-	// 	}
-	// }
-
 	return (
 		<main className="App">
 			<Container>
+				<Row>
+				<Col>
+				<div className ="leftPanel"></div>
+				</Col>
+				<Col xs={10}>
 				<Row>
 					<Col>
 						<h1 className="heading-stlye">Chuck Jokes</h1>
@@ -81,7 +77,21 @@ function App() {
 				</Row>
 				<Row>
 					<Col>
-						<DropdownButton
+					<div className="button-style">
+							{ <Button variant="secondary" onClick={() => {
+									check =true;
+								 	randomJoke?.randomJoke ? setCategory({name: randomJoke?.randomJoke?.category[0]}) : setCategory({name: "dev"})
+									console.log("I clicked a button");
+							}}>get joke</Button> }
+						</div>
+					</Col>
+					<Col>
+					<div className ="spinner-style">
+					{!showSpinner ? "" : randomJoke?.randomJoke ? <h3>{randomJoke?.randomJoke?.category[0]}</h3> : <Spinner animation="grow"/> }
+					</div>
+					</Col>
+					<Col>
+					<DropdownButton
 							id="dropdown-basic-button"
 							title="Select Category"
 							className="drop-down-style"
@@ -89,35 +99,32 @@ function App() {
 						>
 							{data.categories.map((jokeCategory) => (
 								<Dropdown.Item onClick={() => { 
-								
+									check =true;
 									setCategory(jokeCategory);
-								
-									
 								}}>
-									{JSON.stringify(jokeCategory)}
+									{jokeCategory.name}
 								</Dropdown.Item>
 							))}
 						</DropdownButton>
+
 					</Col>
 				</Row>
 				<Row>
-					<Col md={4}></Col>
-					<Col md={4}>
+					{/* <Col md={4}></Col> */}
+					<Col>
 						<div className="divParagraphStyle">
 							<p className="jokeParagraphStyle">
 								{randomJoke?.randomJoke?.content}
 							</p>
 						</div>
-						<div>
-							<h1>{randomJoke?.randomJoke?.category[0]}</h1>
-							{ <button onClick={() => {
-									check =true;
-								 	setCategory({name: randomJoke?.randomJoke?.category[0]});
-									console.log("I clicked a button");
-							}}>get joke</button> }
-						</div>
 					</Col>
-					<Col md={4}></Col>
+					{/* <Col md={4}></Col> */}
+				</Row>
+				</Col>
+									<Col className="rightPanel">
+										<div className="rightPanel"></div>
+									</Col>
+									
 				</Row>
 			</Container>
 		</main>
